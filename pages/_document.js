7a7687e4515bd4f0e0219a0244f41265782bef71
rightson <chari.rightson@gmail.com>
2023-01-19 20:58:17 +0300

    1-3

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Atomic+Age&family=Gloria+Hallelujah&family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
                         rel="stylesheet"
